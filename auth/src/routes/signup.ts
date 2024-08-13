e05a7ed164f0d58341de8a12d5fdf1bc49a5d5cb
Graham Lutz <graham.lutz@gmail.com>
2024-08-13 12:08:33 -0400

    signup auth basics

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('hello from the signupRouter');
-});
+router.post(
+  '/api/users/signup', 
+  [
+    body('email')
+      .isEmail()
+      .withMessage('must use valid email address'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 30 })
+      .withMessage('password must be between 4 and 30 characters')
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    console.log('creating user');
+    const { email, password } = req.body;
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
