7ec5a750dcb8a847a9475462bf17da6fe8e05112
Graham Lutz <graham.lutz@gmail.com>
2024-08-14 05:51:09 -0400

    consistent error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error, 
@@ -8,13 +7,8 @@ export const errorHandler = (
   res: Response, 
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     console.log('handling error as validation error');
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    console.log('handling error as database connection error');
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
