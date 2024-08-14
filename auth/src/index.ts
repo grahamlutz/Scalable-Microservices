148101412e3c24e738c7078d434c1e094e027075
Graham Lutz <graham.lutz@gmail.com>
2024-08-14 05:56:18 -0400

    adds not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -19,7 +20,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter)
 
+app.all('*', () => {
+  throw new NotFoundError();
+})
+
 app.use(errorHandler);
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
