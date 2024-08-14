530d26d9db8fd7265856879adf13673d004abce6
Graham Lutz <graham.lutz@gmail.com>
2024-08-14 04:54:33 -0400

    custom error created

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,27 @@
+import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super();
+
+    // Because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  };
+
+  serializeErrors() {
+    let errors = this.errors.map(err => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+
+      return { message: err.msg };
+    });
+
+    console.log(errors);
+    return errors;
+  };
+};
+
