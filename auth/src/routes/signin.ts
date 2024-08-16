8b9d3ae9cede726edb8806408981093c0bedb9dc
Graham Lutz <graham.lutz@gmail.com>
2024-08-16 08:53:52 -0400

    adds request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hello from the signinRouter');
-});
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('email but be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('you must supply a password')
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    // res.send('hello from the signinRouter');
+  }
+);
 
 export { router as signinRouter };
