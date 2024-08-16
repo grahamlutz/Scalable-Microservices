51f62f37180ce9277f48c5c0c96d69367cadc23c
Graham Lutz <graham.lutz@gmail.com>
2024-08-16 09:34:01 -0400

    signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -15,7 +20,34 @@ router.post('/api/users/signin',
       .withMessage('you must supply a password')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials 1');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials 2');
+    }
+
+    // generate JWT 
+    const userJwt = jwt.sign( {
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.JWT_KEY as string);
+
+    // store jwt on session object
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(200).send(existingUser);
+
     // res.send('hello from the signinRouter');
   }
 );
