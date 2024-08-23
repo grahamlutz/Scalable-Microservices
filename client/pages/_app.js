d2b1088f7bbf0593f4a61215918c88a46f2e0067
Graham Lutz <graham.lutz@gmail.com>
2024-08-23 17:05:56 -0400

    handles issue where getInitialprops is different for App wrapper and individual pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Hello, {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  )
 };
 
-export default App;
+// "appContenxt" if different in AppComponent as opposed to individual pages.
+// just a bit of next trivia.
+AppComponent.getInitialProps = async (appContext) => {
+
+  const { data } = await buildClient(appContext.ctx).get('/api/users/currentuser');
+  
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  
+  return {
+    pageProps,
+    ...data
+  };
+}
+
+export default AppComponent;
