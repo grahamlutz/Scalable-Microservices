36d97f686da150acb2e006769e2d003f35318059
Graham Lutz <graham.lutz@gmail.com>
2024-08-23 16:33:29 -0400

    passing headers with nginx routing with kubernetes

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,16 +5,14 @@ const Landing = ({currentUser}) => {
   return <h1>Landing Page</h1>
 };
 
-Landing.getInitialProps = async () => {
+Landing.getInitialProps = async ({ req }) => {
   
   if(typeof window === 'undefined') {
     // we're running on the server!
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          host: 'ticketing.dev'
-        }
+        headers: req.headers
       }
     ).catch(err => console.log(err));
 
