5f5419fcfab2e2828c2cf1074a28d4bbf7eace55
Graham Lutz <graham.lutz@gmail.com>
2024-08-23 16:44:54 -0400

    centralized buildClient for ssr data fetching

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const Landing = ({currentUser}) => {
   console.log('currentUser: ', currentUser);
   return <h1>Landing Page</h1>
 };
 
-Landing.getInitialProps = async ({ req }) => {
-  
-  if(typeof window === 'undefined') {
-    // we're running on the server!
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    ).catch(err => console.log(err));
+Landing.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // we're running on the browser!
-    const { data } = await axios.get('/api/users/currentuser').catch(err => console.log(err));
-
-    return data;
-  }
+  return data;
 }
 
 export default Landing;
