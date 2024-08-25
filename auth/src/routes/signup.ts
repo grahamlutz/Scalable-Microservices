bf8cfe1d62ec61c2c11e5157375dd3f0ebb6d030
Graham Lutz <graham.lutz@gmail.com>
2024-08-25 14:06:57 -0400

    moves auth errors and middleware logic into common package published on npm as @gtl-tix/common

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request';
-import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@gtl-tix/common';
+import { User } from '../models/user';
 
 const router = express.Router();
 
