5c39a675cde16dad40715d3a49cd8cd8784dc6f6
Graham Lutz <graham.lutz@gmail.com>
2024-08-26 15:06:27 -0400

    new ticket route tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,9 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
-  const response = await request(app)
-    .post('/api/tickets')
-    .send({});
+  const response = await request(app).post('/api/tickets').send({});
 
   expect(response.status).not.toEqual(404);
 });
@@ -13,7 +11,7 @@ it('can only be accessed if the user is signed in', async () => {
   await request(app).post('/api/tickets').send({}).expect(401);
 });
 
-it('returns status other than 401 is user is signed in', async () => {
+it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
@@ -28,7 +26,7 @@ it('returns an error if an invalid title is provided', async () => {
     .set('Cookie', global.signin())
     .send({
       title: '',
-      price: 10
+      price: 10,
     })
     .expect(400);
 
@@ -36,7 +34,7 @@ it('returns an error if an invalid title is provided', async () => {
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      price: 10
+      price: 10,
     })
     .expect(400);
 });
@@ -46,8 +44,8 @@ it('returns an error if an invalid price is provided', async () => {
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'Tada',
-      price: -10
+      title: 'asldkjf',
+      price: -10,
     })
     .expect(400);
 
@@ -55,11 +53,9 @@ it('returns an error if an invalid price is provided', async () => {
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'Tada',
+      title: 'laskdfj',
     })
     .expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {
-  
-});
+it('creates a ticket with valid inputs', async () => {});
