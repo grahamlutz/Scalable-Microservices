880ee81e78fc0e0baf3faa98679251c794127a42
Graham Lutz <graham.lutz@gmail.com>
2024-08-26 06:31:38 -0400

    boilerplate for tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import 'express-async-errors';
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@gtl-tix/common";
+
+const app = express();
+
+// trust traffic from nginx proxy
+app.set('trust proxy', true);
+
+// Middlewares
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+)
+
+// Routes
+
+app.all('*', async () => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
