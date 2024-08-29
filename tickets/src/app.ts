6dbb179455b5008a6785f513c033f7f0cdc472be
Graham Lutz <graham.lutz@gmail.com>
2024-08-29 14:43:04 -0400

    update route and tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from "@gtl-tix/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from './routes/show';
 import { getTicketsRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(getTicketsRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
