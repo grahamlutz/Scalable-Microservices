1fa85797697f37c4fbd783e2579a458878e1b4cb
Graham Lutz <graham.lutz@gmail.com>
2024-09-02 11:32:21 -0400

    orders index route with test

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError, BadRequestError } from '@gtl-tix/common';
+import { Order } from '../models/order';
 
 
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('ticket');
 
-    res.send({});
+  res.send(orders);
 });
 
 export { router as indexOrdersRouter };
