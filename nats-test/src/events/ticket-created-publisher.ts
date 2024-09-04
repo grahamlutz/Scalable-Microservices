fc4292df36a6261cbf6a1965c075a34808936883
Graham Lutz <graham.lutz@gmail.com>
2024-09-04 06:19:46 -0400

    removes nats-test folder

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, TicketCreatedEvent, Subjects } from '@gtl-tix/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
