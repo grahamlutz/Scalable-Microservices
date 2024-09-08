845906ec13a894be3a6e8eb96ee591ecd7d1a2c8
Graham Lutz <graham.lutz@gmail.com>
2024-09-08 13:50:24 -1000

    updates path to openapi.json

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-  const spec = require('../openapi.json');
+  const spec = require('openapi.json');
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     spec,
