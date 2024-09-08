aae08921b02d18f0b400c633cbda2b82a518b7be
Graham Lutz <graham.lutz@gmail.com>
2024-09-08 13:48:05 -1000

    trying new spec

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -1,9 +1,10 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-
+  const spec = require('../openapi.json');
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../openapi.json",
+    spec,
+    url: "openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
