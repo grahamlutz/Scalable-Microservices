297b83a43112e65daae48f5333c67327973283ca
Graham Lutz <graham.lutz@gmail.com>
2024-09-08 14:48:03 -1000

    removes spec altogether

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -1,9 +1,8 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-  // const spec = require('openapi.json');
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+
+
   window.ui = SwaggerUIBundle({
-    // spec,
     url: "openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
